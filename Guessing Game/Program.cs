b9df822a688d35a80a6f636e75193b4d1a2aa1c0
DavidRose <DavidRose331@Gmail.com>
2023-05-07 10:11:14 -0500

    initial

---
+++ Guessing Game/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+
+    static void Mian(string[] args)
+    {
+
+        string secretWord = "Basketball";
+        string guess = "";
+        int guessCount = 0;
+        int guessLimit = 5;
+        bool outOfGuesses = false;
+
+
+        while (guess != secretWord && !outOfGuesses)
+        {
+            if (guessCount < guessLimit)
+            {
+                Console.Write("Enter Guess: ");
+                guess = Console.ReadLine();
+                guessCount++;
+            }
+            else
+            {
+                outOfGuesses = true;
+            }
+            
+            
+        }
+        if (outOfGuesses)
+        {
+            Console.Write("You Lose!");
+        }
+        else
+        {
+            Console.Write("You Win!");
+        }
+        
+
+
+    }
+
+}
+
+    
